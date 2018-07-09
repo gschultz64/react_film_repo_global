8f2fd9ccf76f5c3d695cc2a6d5c67836776fa1da
Genevieve Schultz <genschultz@gmail.com>
2018-07-09 10:41:24 -0700

    refactored Fave

--- src/Fave.js
+++ src/Fave.js
@@ -1,26 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Fave extends Component {
-  
-  constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this)
-  }
+const Fave = props =>  {
 
-  handleClick(e) {
+  function handleClick(e) {
     e.stopPropagation();
-    this.props.onFaveToggle()
+    props.onFaveToggle()
   }
-  
-  render() {
-    const isFave = (this.props.isFave) ? 'remove_from_queue' : 'add_to_queue'
 
-    return(
-      <div className={"film-row-fave " + isFave} onClick={(e) => this.handleClick(e)}>
-        <p className="material-icons">{isFave}</p>
-      </div>
-    )
-  }
+  const isFave = (props.isFave) ? 'remove_from_queue' : 'add_to_queue'
+  
+  return(
+    <div className={"film-row-fave " + isFave} onClick={(e) => handleClick(e)}>
+      <p className="material-icons">{isFave}</p>
+    </div>
+  )
 }
 
 export default Fave
